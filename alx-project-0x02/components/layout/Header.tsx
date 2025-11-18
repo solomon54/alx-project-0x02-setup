7903a36f9e3b4e02379bb13a5b4419f4614f8380
solomon54 <solomontsehay54@gmail.com>
2025-11-18 23:33:50 +0300

    basic routing in Next.js

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,7 +1,18 @@
+import React from 'react'
+import Link from 'next/link';
+
 export default function Header() {
     return (
-        <header className="bg-blue-600 text-white p-4">
-            <h1 className="text-3xl font-bold">My Application</h1>
+       <main>
+        <header className="bg-gray-800 text-white p-4">
+            <nav className="container mx-auto flex justify-between">
+                <div className="text-lg font-bold">MyWebsite</div>
+                <div className="space-x-4">
+                    <Link href="/home" className="hover:underline">Home</Link>
+                    <Link href="/about" className="hover:underline">About</Link>
+                </div>
+            </nav>
         </header>
+       </main>
     )
 }
