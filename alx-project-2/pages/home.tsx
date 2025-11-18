84065a180195e02ebea4be82ba9679fe89564722
solomon54 <solomontsehay54@gmail.com>
2025-11-18 22:51:36 +0300

    add some componnent structure

---
+++ alx-project-2/pages/home.tsx
@@ -0,0 +1,8 @@
+export default function Home(){
+    return (
+        <div className="text-2xl text-gray-700 bg-gray-100">
+            <h1>Home Page</h1>
+            <p>This is the home page...</p>
+        </div>
+    )
+}
