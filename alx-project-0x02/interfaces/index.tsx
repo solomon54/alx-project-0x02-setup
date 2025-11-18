5bfe2534bebb274a76e9c7f5db1dec4cecdad87f
solomon54 <solomontsehay54@gmail.com>
2025-11-18 23:06:45 +0300

    add the missing component

--- alx-project-0x02/interfaces/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Index(){
-    return (
-        <div>
-            <h1>Index Page</h1>
-            <p>Welcome to the index page of our application.</p>
-        </div>
-    )
-}
