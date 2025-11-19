16b0e5c2c97fadcacb1eb719c44915e19cd90fb1
solomon54 <solomontsehay54@gmail.com>
2025-11-20 01:29:33 +0300

    Create and reuse the button cmponnent

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,4 +1,4 @@
-import { ButtonProps } from "@/interfaces";
+import { type ButtonProps } from "@/interfaces";
 export default function Button({size, shape}:ButtonProps){
     let padding;
     let borderRadius;
