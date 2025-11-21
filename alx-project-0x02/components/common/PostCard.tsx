b1138a2ea35c6eb9b0605e70fcdafce634a8cffc
solomon54 <solomontsehay54@gmail.com>
2025-11-21 21:18:06 +0300

    impelement a postcard component

--- alx-project-0x02/components/common/PostCard.tsx
+++ alx-project-0x02/components/common/PostCard.tsx
@@ -3,13 +3,13 @@ import { type PostProps } from "@/interfaces";
 import { Content } from "next/font/google";
 export default function PostCard({title , imgUrl, userId, content}:PostProps) {
     return (
-        <div className="">
-            <div className=" ml-5 text-center mt-5 p-3 justify-center bg-blue-200 shadow-2xl shadow-emerald-400 rounded-b-md hover:scale-101 cursor">
-            <Image src ={imgUrl}  alt='image post' width={400} height={300} className="shadow-[5xl]"></Image>
-            <h2 className="text-2xl text-gray-900 font-bold  py-2">{title} <span className="block w-1/6 font-semibold text-sm p-2 shadow-2xl bg-indigo-300 rounded-full">userId:{userId}</span></h2>
-            <p className="text-lg text-gray-700 font-semibold  py-4">{content} </p>
+        <div className="block">
+            <div className="block ml-5 text-center mt-5 p-3 justify-center bg-blue-200 shadow-2xl shadow-emerald-400 rounded-b-md hover:scale-101 cursor">
+                <Image src ={imgUrl}  alt='image post' width={400} height={300} className="shadow-[5xl]"></Image>
+                <h2 className="text-2xl text-gray-900 font-bold  py-2">{title} <span className="block w-1/6 font-semibold text-sm p-2 shadow-2xl bg-indigo-300 rounded-full">userId:{userId}</span></h2>
+                <p className="text-lg text-gray-700 font-semibold  py-4">{content} </p>
 
-        </div>
+            </div>
         </div>
     )
 }
--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,11 +1,29 @@
 import Header from "@/components/layout/Header"
 import Posts from "@/components/common/PostCard"
-export default function PostPage(){
+
+
+export async function getStaticProps() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await res.json();
+    return({
+        props: {
+            posts,
+        },
+    })
+}
+export default function PostPage({posts}){
     return(
         <div >
             <Header />
             <h1 className="text-3xl text-gray-800 font-bold  underline underline-offset-4" >Posts Page</h1>
 <div className="flex gap-2 ">
+
+{/* from API */}
+        {posts.slice(0, 6).map((post) =>{
+            return <Posts key={post.id} title={post.title} userId={post.userId} content={post.body} />
+        })}
+
+
     <Posts title='Ultmate React course...' userId='1' content="The Ulatimate React course is one of the best common tech worlds Leader Carior skill..."  imgUrl="/assets/images/react.png"/>
     <Posts title='Ultmate Next.js course...'  userId='2' content="The Ulatimate Next.js course is one of the best common tech worlds Leader Carior skill..."  imgUrl="/assets/images/next.png"/>
     <Posts title='Ultmate Typscript course...' userId='3'  content="The Ulatimate Typscript course is one of the best common tech worlds Leader Carior skill..." imgUrl="/assets/images/ts.png"/>
