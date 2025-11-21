7bb0a342cc87096ee662be77a056e42799970a49
solomon54 <solomontsehay54@gmail.com>
2025-11-21 21:26:51 +0300

    impelement a postcard component

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -5,23 +5,26 @@ import Posts from "@/components/common/PostCard"
 export async function getStaticProps() {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
     const posts = await res.json();
-    return({
+    return{
         props: {
             posts,
         },
-    })
+    }
 }
 export default function PostPage({posts}){
     return(
         <div >
             <Header />
             <h1 className="text-3xl text-gray-800 font-bold  underline underline-offset-4" >Posts Page</h1>
-<div className="flex gap-2 ">
-
-{/* from API */}
+<div className="grid grid-cols-3 gap-2 ">{/* from API */}
         {posts.slice(0, 6).map((post) =>{
+            console.log(post);
             return <Posts key={post.id} title={post.title} userId={post.userId} content={post.body} />
-        })}
+        })}</div>
+        
+<div className="grid grid-cols-4 gap-2 ">
+
+
 
 
     <Posts title='Ultmate React course...' userId='1' content="The Ulatimate React course is one of the best common tech worlds Leader Carior skill..."  imgUrl="/assets/images/react.png"/>
